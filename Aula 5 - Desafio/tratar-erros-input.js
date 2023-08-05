7580492b244815fc8b575b93520ee5525909234a
Diogo Melo <diogomelo500@gmail.com>
2023-08-05 13:01:42 -0300

    tratando erros de input de nome/notas

--- Aula 5 - Desafio/tratar-erros-input.js
+++ Aula 5 - Desafio/tratar-erros-input.js
@@ -6,27 +6,27 @@ while (!aluno.nome || !aluno.turma || !aluno.notas) { // verifica se todos os ca
 
 
     const nome = input.question('Informe seu nome: ')
-    if (!nome.length || !nome.match(/[^0-9]/g)) { // verifica se nome contém números ou seu formato é inválido
-        console.log(!nome.length)
+    if (!nome.length || nome.split('').filter(Boolean).find(n => /\d/.test(n))) { // verifica se nome contém números ou seu formato é inválido
         console.log(`O nome ${nome} é invalido.\nTente novamente.`)
         continue
     }
-    const turma = input.question('Informe sua turma: ')
+
+    const turma = input.question('Informe o numero da sua turma: ')
     if (!turma.length) {  // verifica se turma não contém caracteres
         console.log(`A turma ${turma} é invalida.\nTente novamente.`)
         continue
-
     }
+    
     const notas = []
     const qtdeNotas = input.question('Informe a quantidade de notas: ')
-    if (!qtdeNotas || qtdeNotas.search(RegExp(/[^a-zA-Z]/))) { // verifica se a quantidade de notas é inválida
+    if (!qtdeNotas || isNaN(qtdeNotas)) { // verifica se a quantidade de notas é inválida
         console.log(`A quantidade de notas ${qtdeNotas} é inválida.\nTente novamente.`)
         continue
     }
     for (let i = 0; i < qtdeNotas; i++) {
         const nota = Number(input.question(`Informe a nota ${i + 1}: `))
-        if (isNaN || nota < 0 || nota > 10) { // verifica se a nota é inválida
-            console.log(`A nota ${nota} é invalida.\nTente novamente.`)
+        if (isNaN(nota) || nota < 0 || nota > 10) { // verifica se a nota é inválida
+            console.log(`A nota informada é invalida.\nTente novamente.`)
             i--
             continue
         }
@@ -35,7 +35,7 @@ while (!aluno.nome || !aluno.turma || !aluno.notas) { // verifica se todos os ca
     aluno = {
         nome,
         turma,
-        notas: notas.split(',').map(Number)
+        notas
     }
 }
 
