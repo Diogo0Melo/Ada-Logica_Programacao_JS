ca4c4751092dfc6218fbc49d74a03035c9e5f000
Diogo Melo <diogomelo500@gmail.com>
2023-08-05 13:33:24 -0300

    utilizando função

--- Aula 5 - Desafio/tratar-erros-input.js
+++ Aula 5 - Desafio/tratar-erros-input.js
@@ -2,41 +2,50 @@ const input = require('readline-sync')
 
 let aluno = {}
 
-while (!aluno.nome || !aluno.turma || !aluno.notas) { // verifica se todos os campos foram preenchidos
-
-
-    const nome = input.question('Informe seu nome: ')
-    if (!nome.length || nome.split('').filter(Boolean).find(n => /\d/.test(n))) { // verifica se nome contém números ou seu formato é inválido
-        console.log(`O nome ${nome} é invalido.\nTente novamente.`)
-        continue
-    }
-
-    const turma = input.question('Informe o numero da sua turma: ')
-    if (!turma.length) {  // verifica se turma não contém caracteres
-        console.log(`A turma ${turma} é invalida.\nTente novamente.`)
-        continue
-    }
-    
-    const notas = []
-    const qtdeNotas = input.question('Informe a quantidade de notas: ')
-    if (!qtdeNotas || isNaN(qtdeNotas)) { // verifica se a quantidade de notas é inválida
-        console.log(`A quantidade de notas ${qtdeNotas} é inválida.\nTente novamente.`)
-        continue
-    }
-    for (let i = 0; i < qtdeNotas; i++) {
-        const nota = Number(input.question(`Informe a nota ${i + 1}: `))
-        if (isNaN(nota) || nota < 0 || nota > 10) { // verifica se a nota é inválida
-            console.log(`A nota informada é invalida.\nTente novamente.`)
-            i--
+const pegarNome = () => { 
+    while (true) {
+        const nome = input.question('Informe seu nome: ')
+        if (!nome.length || nome.split('').filter(Boolean).find(n => /\d/.test(n))) { // verifica se nome contém números ou está vazio
+            console.log(`O nome ${nome} e invalido.\nTente novamente.`)
             continue
         }
-        notas.push(nota)
-    }
-    aluno = {
-        nome,
-        turma,
-        notas
+        return nome
     }
 }
+
+const pegarTurma = () => {
+    while (true) {
+        const turma = input.question('Informe sua turma: ')
+        if (!turma.length) {  // verifica se turma está vazia
+            console.log(`A turma ${turma} e invalida.\nTente novamente.`)
+            continue
+        }
+        return turma
+    }
+}
+const pegarNotas = () => {
+    while (true) {
+        const notas = []
+        const qtdeNotas = input.question('Informe a quantidade de notas: ')
+        if (!qtdeNotas || isNaN(qtdeNotas)) { // verifica se a quantidade de notas é inválida
+            console.log(`A quantidade de notas ${qtdeNotas} é inválida.\nTente novamente.`)
+            continue
+        }
+        for (let i = 0; i < qtdeNotas; i++) {
+            const nota = Number(input.question(`Informe a nota ${i + 1}: `))
+            if (isNaN(nota) || nota < 0 || nota > 10) { // verifica se a nota é inválida
+                console.log(`A nota informada é invalida.\nTente novamente.`)
+                i--
+                continue
+            }
+            notas.push(nota)
+        }
+        return notas
+    }
+}
+
+aluno.nome = pegarNome()
+aluno.turma = pegarTurma()
+aluno.notas = pegarNotas()
 
 console.log(aluno)
