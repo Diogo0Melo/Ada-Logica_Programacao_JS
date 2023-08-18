083299abee8341c6e2fee3fa4821cbb4bab4fe74
Diogo Melo <diogomelo500@gmail.com>
2023-08-17 21:50:01 -0300

    criando classe Aluno e método cadastrarAluno

--- Testando POO no Projeto Final/projeto_final_com_POO.js
+++ Testando POO no Projeto Final/projeto_final_com_POO.js
@@ -10,9 +10,9 @@ class Turma {
 
     cadastrarTurma() {
         const idTurma = +prompt("Informe o ID da turma");
-        if (verificacoes({idTurma: idTurma}) === null) throw "Programa encerrado!";
+        if (validacoes({ idTurma: idTurma }) === null) throw "Programa encerrado!";
         const maximoDeAlunos = +prompt(`Informe o número máximo de alunos para a turma ${idTurma}. [Entre 5 e 10]`);
-        if (verificacoes({maximoDeAlunos: maximoDeAlunos}) === null) throw "Programa encerrado!";
+        if (validacoes({ maximoDeAlunos: maximoDeAlunos }) === null) throw "Programa encerrado!";
         return [idTurma, maximoDeAlunos];
     }
     mostrarTurma() {
@@ -29,12 +29,12 @@ class Turma {
         switch (opcao) {
             case 1:
                 const novoIdTurma = +prompt(`Informe o novo ID da turma ${this.idTurma}. [Entre 1 e 10]`);
-                if (verificacoes({idTurma: novoIdTurma}) === null) throw "Programa encerrado!";
+                if (validacoes({ idTurma: novoIdTurma }) === null) throw "Programa encerrado!";
                 this.idTurma = novoIdTurma;
                 break;
             case 2:
                 const novoMaximoDeAlunos = +prompt(`Informe o novo número máximo de alunos para a turma ${this.idTurma}. [Entre 5 e 10]`);
-                if (verificacoes({maximoDeAlunos: novoMaximoDeAlunos}) === null) throw "Programa encerrado!";
+                if (validacoes({ maximoDeAlunos: novoMaximoDeAlunos }) === null) throw "Programa encerrado!";
                 this.maximoDeAlunos = novoMaximoDeAlunos;
                 break;
             default:
@@ -43,17 +43,39 @@ class Turma {
     }
     removerTurma() {
         const turmaIndex = turmas[turmas.findIndex((t) => t.idTurma == this.idTurma)];
-        if (this.alunos.length > 0)  throw `A turma ${this.idTurma} não pode ser removida pois possui alunos.`;
+        if (this.alunos.length > 0) throw `A turma ${this.idTurma} não pode ser removida pois possui alunos.`;
         turmas.splice(turmaIndex, 1);
     }
 }
-function turma() {
+class Aluno {
+    constructor() {
+        const [nome, sobrenome, email, turma, classificacao, nascimento, notas, ativo] = this.cadastrarAluno();
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.turma = turma;
+        this.classificacao = classificacao;
+        this.nascimento = nascimento;
+        this.notas = notas;
+        this.ativo = ativo;
+    }
+    cadastrarAluno(){
+        const mensagemDeErro = "Programa encerrado!\nNenhum aluno foi cadastrado.";
+        const nome = prompt("Informe o nome do aluno");
+        if(validacoes({nome: nome}) === null) throw mensagemDeErro;
+        console.log(`O nome ${nome} foi cadastrado com sucesso.`);
+        const sobrenome = prompt("Informe o sobrenome do aluno").replace(/\s+/g, " ");
+        if(validacoes({sobrenome: sobrenome}) === null) throw mensagemDeErro;
+        console.log(`O sobrenome ${sobrenome}foi cadastrado com sucesso.`);
+    }
+}
+function localizarTurma() {
     const idTurma = +prompt("Informe o ID da turma");
     const turma = turmas.find((t) => t.idTurma == idTurma);
     if (turma === undefined) throw "Turma não encontrada";
     return turma;
 }
-function verificacoes({idTurma, maximoDeAlunos, nome, sobrenome, email, turma, classificacao, nascimento, notas, ativo}) {
+function validacoes({ idTurma, maximoDeAlunos, nome, sobrenome, email, turma, classificacao, nascimento, notas, ativo }) {
     if (idTurma !== undefined) {
         if (isNaN(idTurma) || idTurma < 1 || idTurma > 10) {
             console.warn(`A turma ${idTurma} é inválida.`);
@@ -68,9 +90,18 @@ function verificacoes({idTurma, maximoDeAlunos, nome, sobrenome, email, turma, c
             return null;
         } else return maximoDeAlunos;
     } else if (nome !== undefined) {
-        return nome;
+        const regex = /^[a-zA-ZéáàçõãúíÉÁÀÇÕÃÚÍ]+$/g;
+        if (regex.test(nome) && nome !== null){
+            const nomeFormatado = nome.slice(0, 1).toUpperCase() + nome.slice(1).toLowerCase();
+            return nomeFormatado;
+        }
+        else return null
     } else if (sobrenome !== undefined) {
-        return sobrenome;
+        const regex = /^[a-zA-Z' 'éáàçõãúíÉÁÀÇÕÃÚÍ]+$/g;
+        if (regex.test(sobrenome) && sobrenome !== null){
+            const sobrenomeFormatado = sobrenome.toLowerCase();
+            return sobrenomeFormatado;
+        }
     } else if (email !== undefined) {
         return email;
     } else if (turma !== undefined) {
